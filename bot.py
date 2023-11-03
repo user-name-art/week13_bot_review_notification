2de0ca2fa4c2a9a65c1d6400a454a15704d72cfa
Art Gilyazov <art.gilyazov@mail.ru>
2023-11-03 16:38:55 +0300

    fix logging

--- bot.py
+++ bot.py
@@ -6,9 +6,6 @@ import requests
 from environs import Env
 
 
-URL = 'https://dvmn.org/api/long_polling/'
-
-
 def get_lesson_review(server_answer):
     lesson_title = server_answer['new_attempts'][0]['lesson_title']
     lesson_url = server_answer['new_attempts'][0]['lesson_url']
@@ -25,30 +22,22 @@ def main():
     env = Env()
     env.read_env()
 
-    logger = logging.getLogger('ReviewNotifications')
-    logger.setLevel(logging.INFO)
-    
-    handler = logging.FileHandler('logs.log')
- 
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
-    
-    logger.addHandler(handler)
+    logging.basicConfig(level=logging.INFO, filename='log.log', format='%(asctime)s %(levelname)s %(message)s')
 
+    URL = 'https://dvmn.org/api/long_polling/'
     BOT_TOKEN = env.str('TG_BOT_TOKEN')
     CHAT_ID = env.str('TG_CHAT')
     DEVMAN_TOKEN = env.str('DEVMAN_TOKEN')
     DEVMAN_TIMEOUT = env.int('DEVMAN_TIMEOUT', 100)
     
-    params = {'timeout': DEVMAN_TIMEOUT}
+    params = {}
     headers = {'Authorization': f'Token {DEVMAN_TOKEN}'}
 
     bot = telegram.Bot(token=BOT_TOKEN)
 
     while True:
         try:
-            logger.info('start!')
-            response = requests.get(URL, headers=headers, params=params)
+            response = requests.get(URL, headers=headers, params=params, timeout=DEVMAN_TIMEOUT)
             response.raise_for_status()
 
             server_answer = response.json()
@@ -57,6 +46,7 @@ def main():
                 params['timestamp'] = server_answer['timestamp_to_request']
             elif server_answer['status'] == 'found':
                 params['timestamp'] = server_answer['last_attempt_timestamp']
+                logging.info('Статус проверки работы изменился')
                 
                 lesson_title, lesson_url, lesson_assessment = get_lesson_review(server_answer)
 
@@ -65,10 +55,10 @@ def main():
                                 )
 
         except requests.exceptions.ReadTimeout:
-            logger.info('time out')
+            logging.error('ReadTimeout')
         except requests.exceptions.ConnectionError:
-            logger.info('ConnectionError')
-            time.sleep(2)
+            logging.error('ConnectionError')
+            time.sleep(30)
 
 
 if __name__ == '__main__':
