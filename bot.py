406fb300ad361fd56ff07fb229fe46354fc2f01d
Art Gilyazov <art.gilyazov@mail.ru>
2023-11-06 17:47:33 +0300

    fix variable names

--- bot.py
+++ bot.py
@@ -6,11 +6,14 @@ import requests
 from environs import Env
 
 
-def get_lesson_summary(server_answer):
-    lesson_title = server_answer['new_attempts'][0]['lesson_title']
-    lesson_url = server_answer['new_attempts'][0]['lesson_url']
+DEVMAN_URL = 'https://dvmn.org/api/long_polling/'
 
-    if server_answer['new_attempts'][0]['is_negative']:
+
+def get_lesson_summary(response_object):
+    lesson_title = response_object['new_attempts'][0]['lesson_title']
+    lesson_url = response_object['new_attempts'][0]['lesson_url']
+
+    if response_object['new_attempts'][0]['is_negative']:
         lesson_assessment = 'К сожалениию, в работе нашлись ошибки.'
     else:
         lesson_assessment = 'Преподавателю все понравилось, можно приступать к следующему уроку.'
@@ -24,33 +27,32 @@ def main():
 
     logging.basicConfig(level=logging.INFO, filename='log.log', format='%(asctime)s %(levelname)s %(message)s')
 
-    URL = 'https://dvmn.org/api/long_polling/'
-    BOT_TOKEN = env.str('TG_BOT_TOKEN')
-    CHAT_ID = env.str('TG_CHAT')
-    DEVMAN_TOKEN = env.str('DEVMAN_TOKEN')
-    DEVMAN_TIMEOUT = env.int('DEVMAN_TIMEOUT', 100)
+    bot_token = env.str('TG_BOT_TOKEN')
+    chat_id = env.str('TG_CHAT')
+    devman_token = env.str('DEVMAN_TOKEN')
+    devman_timeout = env.int('DEVMAN_TIMEOUT', 100)
     
     params = {}
-    headers = {'Authorization': f'Token {DEVMAN_TOKEN}'}
+    headers = {'Authorization': f'Token {devman_token}'}
 
-    bot = telegram.Bot(token=BOT_TOKEN)
+    bot = telegram.Bot(token=bot_token)
 
     while True:
         try:
-            response = requests.get(URL, headers=headers, params=params, timeout=DEVMAN_TIMEOUT)
+            response = requests.get(DEVMAN_URL, headers=headers, params=params, timeout=devman_timeout)
             response.raise_for_status()
 
-            server_answer = response.json()
+            response_object = response.json()
 
-            if server_answer['status'] == 'timeout':
-                params['timestamp'] = server_answer['timestamp_to_request']
-            elif server_answer['status'] == 'found':
-                params['timestamp'] = server_answer['last_attempt_timestamp']
+            if response_object['status'] == 'timeout':
+                params['timestamp'] = response_object['timestamp_to_request']
+            elif response_object['status'] == 'found':
+                params['timestamp'] = response_object['last_attempt_timestamp']
                 logging.info('Статус проверки работы изменился')
                 
-                lesson_summary = get_lesson_summary(server_answer)
+                lesson_summary = get_lesson_summary(response_object)
 
-                bot.send_message(chat_id=CHAT_ID, text=lesson_summary)
+                bot.send_message(chat_id=chat_id, text=lesson_summary)
 
         except requests.exceptions.ReadTimeout:
             logging.error('ReadTimeout')
