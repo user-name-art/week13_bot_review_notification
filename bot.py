e6f7725baf957809daf4b4e5efb69b97e00653bb
Art Gilyazov <art.gilyazov@mail.ru>
2023-12-05 06:43:04 +0300

    catch errors and write to log

--- bot.py
+++ bot.py
@@ -9,6 +9,19 @@ from environs import Env
 DEVMAN_URL = 'https://dvmn.org/api/long_polling/'
 
 
+class TelegramLogsHandler(logging.Handler):
+
+    def __init__(self, bot_token, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.bot_token = bot_token
+        self.tg_bot = telegram.Bot(token=bot_token)
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
+
+
 def get_lesson_summary(review_summary):
     lesson_title = review_summary['new_attempts'][0]['lesson_title']
     lesson_url = review_summary['new_attempts'][0]['lesson_url']
@@ -25,18 +38,20 @@ def main():
     env = Env()
     env.read_env()
 
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
-
     bot_token = env.str('TG_BOT_TOKEN')
     chat_id = env.str('TG_CHAT')
     devman_token = env.str('DEVMAN_TOKEN')
     devman_timeout = env.int('DEVMAN_TIMEOUT', 100)
+
+    logger = logging.getLogger('Logger')
+    logger.setLevel(logging.INFO)
+    logger.addHandler(TelegramLogsHandler(bot_token, chat_id))
     
     params = {}
     headers = {'Authorization': f'Token {devman_token}'}
 
     bot = telegram.Bot(token=bot_token)
-    logging.info('bot started')
+    logger.info('bot started')
 
     while True:
         try:
@@ -49,17 +64,18 @@ def main():
                 params['timestamp'] = review_summary['timestamp_to_request']
             elif review_summary['status'] == 'found':
                 params['timestamp'] = review_summary['last_attempt_timestamp']
-                logging.info('Статус проверки работы изменился')
-                
+                logger.info('Статус проверки работы изменился')
                 lesson_summary = get_lesson_summary(review_summary)
-
                 bot.send_message(chat_id=chat_id, text=lesson_summary)
 
         except requests.exceptions.ReadTimeout:
-            logging.error('ReadTimeout')
+            logger.error('ReadTimeout')
         except requests.exceptions.ConnectionError:
-            logging.error('ConnectionError')
+            logger.error('ConnectionError')
             time.sleep(30)
+        except Exception as err:
+            logger.error('Возникла следующая ошибка:')
+            logger.error(err, exc_info=True)
 
 
 if __name__ == '__main__':
